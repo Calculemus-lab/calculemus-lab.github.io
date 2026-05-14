86d5e52204d39545d50703bc992822daf33aa8f1
R.Berger <r.m.berger@umail.leidenuniv.nl>
2026-05-14 08:43:34 +0200

    Style update, logos included

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Calculemus AI';
+export const SITE_TITLE = 'Calculemus Team';
 export const SITE_DESCRIPTION = 'Leiden University AI Team';
